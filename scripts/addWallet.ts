4b8413929f41d985c27852e0807282ca23355fd2
talhareh <talha59@gmail.com>
2024-08-15 22:21:24 +0400

    escrow start

--- scripts/addWallet.ts
+++ scripts/addWallet.ts
@@ -5,31 +5,63 @@ import { Address, toNano } from "@ton/core";
 
 export async function run(provider:NetworkProvider){
 
-    const ruff = provider.open(Ruffruff.fromAddress(Address.parse('EQB7vFlIfwZvW2uJWkBZmYTMNE5XiAPmGVE-Sq5MrWKlYWpW')));
-    const addr = Address.parse('EQDa7mjzjXAfkKgqoFANUJlzYZoUL69cItzS2pSpPMOH7gsd')
-    const listBefore = await ruff.getGetSubmittedWallets()
-    console.log("List in start : ", listBefore)
+    const ruff = provider.open(Ruffruff.fromAddress(Address.parse('EQC7AGTh1uEb3yLVGQn699LR0dEvbSKjiiry6n6qFIF1Styh')));
+    
+    // const addr = Address.parse('UQBTzIdjCd753CUT1Itz46dGBCWb3W69BvPf9E5UQ2eW-DTI')
+    // const listBefore = await ruff.getSubmittedWallets()
+    // console.log("List in start : ", listBefore)
+
+    // await ruff.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano("0.01")
+    //     },
+    //     {
+    //         $$type: "SubmitWallet",
+    //         address:addr,
+            
+    //     }
+    // )
+
+    // let listAfter = await ruff.getSubmittedWallets()
+    // console.log("Wallet added : ", listAfter)
+
+
+
+    // 2. Set the holding wallet address
+    //const holdingWalletAddr = Address.parse('EQCAMqa2TlkJCFVlekQZjAqMP-lGuxFO9fjVu6aAA27r046z');
+    
+    // console.log("Setting holding wallet address:", holdingWalletAddr);
+    // await ruff.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano("0.01")
+    //     },
+    //     {
+    //         $$type: "SetHoldingWallet",
+    //         address: holdingWalletAddr,
+    //     }
+    // );
+
+    // Check that the holding wallet is set
+    // const currentHoldingWallet = await ruff.getHoldingWallet();
+    // console.log("Current holding wallet:", currentHoldingWallet);
+
+    // 3. Transfer tokens to the holding wallet
+    const transferAmount = toNano("0.34");
+    console.log(`Transferring ${transferAmount.toString()} tokens to holding wallet`);
 
     await ruff.send(
         provider.sender(),
         {
-            value: toNano("0.05")
+            value: toNano("0.01")
         },
         {
-            $$type: "SubmitWallet",
-            address:addr,
-            
+            $$type: "TransferToHoldingWallet",
+            amount: transferAmount,
         }
-    )
+    );
 
-    let listAfter = await ruff.getGetSubmittedWallets()
-    let attempt = 1
-    while(listAfter === listBefore){
-        console.log('Adding Wallet, attempt#', attempt)
-        await sleep(2000)
-        listAfter = await ruff.getGetSubmittedWallets();
-        attempt++;
+    // console.log("Tokens transferred to holding wallet:", holdingWalletAddr);
 
-    }
-    console.log("Wallet added : ", listAfter)
 }
