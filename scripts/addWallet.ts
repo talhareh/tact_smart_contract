79f3786633b6889868f5be2dfa8f1e92bbcfaad1
talhareh <talha59@gmail.com>
2024-08-21 19:12:52 +0400

    new tests

--- scripts/addWallet.ts
+++ scripts/addWallet.ts
@@ -7,24 +7,29 @@ export async function run(provider:NetworkProvider){
 
     const ruff = provider.open(Ruffruff.fromAddress(Address.parse('EQC7AGTh1uEb3yLVGQn699LR0dEvbSKjiiry6n6qFIF1Styh')));
     
-    // const addr = Address.parse('UQBTzIdjCd753CUT1Itz46dGBCWb3W69BvPf9E5UQ2eW-DTI')
-    // const listBefore = await ruff.getSubmittedWallets()
-    // console.log("List in start : ", listBefore)
+    let copiedAddr = 'UQARmOGIAiYUXv-oYWs4sIpaOOZ_qZ8iys_puTQ1TaEcyxNX'
+    const addr = Address.parse(copiedAddr)
+    console.log({
+                    wallet_passed: copiedAddr,
+                    addr:addr
+     })
+    const listBefore = await ruff.getSubmittedWallets()
+    console.log("List in start : ", listBefore)
 
-    // await ruff.send(
-    //     provider.sender(),
-    //     {
-    //         value: toNano("0.01")
-    //     },
-    //     {
-    //         $$type: "SubmitWallet",
-    //         address:addr,
+    await ruff.send(
+        provider.sender(),
+        {
+            value: toNano("0.01")
+        },
+        {
+            $$type: "SubmitWallet",
+            address:addr,
             
-    //     }
-    // )
+        }
+    )
 
-    // let listAfter = await ruff.getSubmittedWallets()
-    // console.log("Wallet added : ", listAfter)
+    let listAfter = await ruff.getSubmittedWallets()
+    console.log("Wallet added : ", listAfter)
 
 
 
@@ -48,19 +53,19 @@ export async function run(provider:NetworkProvider){
     // console.log("Current holding wallet:", currentHoldingWallet);
 
     // 3. Transfer tokens to the holding wallet
-    const transferAmount = toNano("0.01");
-    // console.log(`Transferring ${transferAmount.toString()} tokens to holding wallet`);
+    // const transferAmount = toNano("0.01");
+    // // console.log(`Transferring ${transferAmount.toString()} tokens to holding wallet`);
 
-    await ruff.send(
-        provider.sender(),
-        {
-            value: transferAmount
-        },
-        {
-            $$type: "TransferToHoldingWallet",
-            amount: transferAmount,
-        }
-    );
+    // await ruff.send(
+    //     provider.sender(),
+    //     {
+    //         value: transferAmount
+    //     },
+    //     {
+    //         $$type: "TransferToHoldingWallet",
+    //         amount: transferAmount,
+    //     }
+    // );
 
     // console.log("Tokens transferred to holding wallet:", holdingWalletAddr);
 
